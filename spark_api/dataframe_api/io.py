18acd57a7bb18ae3347c2e287345d3315272ba20
Ryan Nazareth <ryankarlos@gmail.com>
2022-01-13 23:36:05 +0000

    add example of reading file with schema

--- spark_api/dataframe_api/io.py
+++ spark_api/dataframe_api/io.py
@@ -1,45 +1,13 @@
 from spark_session import create_spark_session
 from pyspark.sql.types import *
 
-spark = create_spark_session("io")
 
-schema = StructType(
-    [
-        StructField("CallNumber", IntegerType(), True),
-        StructField("UnitID", StringType(), True),
-        StructField("IncidentNumber", IntegerType(), True),
-        StructField("CallType", StringType(), True),
-        StructField("CallDate", StringType(), True),
-        StructField("WatchDate", StringType(), True),
-        StructField("CallFinalDisposition", StringType(), True),
-        StructField("AvailableDtTm", StringType(), True),
-        StructField("Address", StringType(), True),
-        StructField("City", StringType(), True),
-        StructField("ZipcodeofIncident", IntegerType(), True),
-        StructField("Battalion", StringType(), True),
-        StructField("StationArea", StringType(), True),
-        StructField("Box", StringType(), True),
-        StructField("OrigPriority", StringType(), True),
-        StructField("Priority", StringType(), True),
-        StructField("FinalPriority", IntegerType(), True),
-        StructField("ALSUnit", BooleanType(), True),
-        StructField("CallTypeGroup", StringType(), True),
-        StructField("NumAlarms", IntegerType(), True),
-        StructField("UnitType", StringType(), True),
-        StructField("UnitSequenceInCallDispatch", IntegerType(), True),
-        StructField("FirePreventionDistrict", StringType(), True),
-        StructField("SupervisorDistrict", StringType(), True),
-        StructField("Neighborhood", StringType(), True),
-        StructField("Location", StringType(), True),
-        StructField("RowID", StringType(), True),
-        StructField("Delay", FloatType(), True),
-    ]
-)
+spark = create_spark_session("io")
 
 
 # Use the DataFrameReader interface to read a CSV file
-def read_dataset_into_df(path, format="parquet", infer_schema=True, schema=None):
-    if infer_schema:
+def read_dataset_into_df(path, format="parquet", schema=None):
+    if schema is None:
         df = (
             spark.read.format(format)
             .option("header", True)
@@ -48,7 +16,6 @@ def read_dataset_into_df(path, format="parquet", infer_schema=True, schema=None)
             .load(path)
         )
     else:
-        assert schema is not None
         df = (
             spark.read.format(format)
             .option("header", True)
@@ -61,10 +28,9 @@ def read_dataset_into_df(path, format="parquet", infer_schema=True, schema=None)
 
 def write_dataset(df, mode, path, table_name=None):
     if mode == "file":
-        df.write.format("parquet")\
-            .mode("overwrite")\
-            .option("compression", "snappy")\
-            .save(path)
+        df.write.format("parquet").mode("overwrite").option(
+            "compression", "snappy"
+        ).save(path)
 
     elif mode == "table":
         if table_name is None:
@@ -80,7 +46,9 @@ if __name__ == "__main__":
     df = read_dataset_into_df(file)
     # create temp table in memory
     df.createOrReplaceTempView("Data")
-    spark.sql("SELECT * FROM data WHERE ORIGIN_COUNTRY_NAME LIKE '%States%' AND  DEST_COUNTRY_NAME='France'").show()
+    spark.sql(
+        "SELECT * FROM data WHERE ORIGIN_COUNTRY_NAME LIKE '%States%' AND  DEST_COUNTRY_NAME='France'"
+    ).show()
     df.unpersist()
 
     # csv
@@ -93,7 +61,24 @@ if __name__ == "__main__":
     df = read_dataset_into_df(file, "json")
     df.show(10)
 
+    # read with schema specified and inferred schema set to false
+
+    schema = StructType(
+        [
+                StructField("DEST_COUNTRY_NAME", StringType(), True),
+                StructField("ORIGIN_COUNTRY_NAME", StringType(), True),
+                StructField("count", LongType(), True),
+        ]
+    )
+    df = read_dataset_into_df(file, "json", schema=schema)
+    df.show(10)
+
     # use DataFrameWriter interface to save dataframe as parquet file
     write_dataset(df, mode="file", path="/tmp/data/parquet/df_parquet")
     # or sql table
-    write_dataset(df, mode="table", path="datasets/flights/spark-warehouse/parquet", table_name="us_delay_flights_tbl")
+    write_dataset(
+        df,
+        mode="table",
+        path="datasets/flights/spark-warehouse/parquet",
+        table_name="us_delay_flights_tbl",
+    )
