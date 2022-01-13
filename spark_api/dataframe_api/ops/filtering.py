908422b42055e1c930c9a2eb1e7be1a59562ea85
Ryan Nazareth <ryankarlos@gmail.com>
2022-01-13 01:27:34 +0000

    add sort filter and limit

--- spark_api/dataframe_api/ops/filtering.py
+++ spark_api/dataframe_api/ops/filtering.py
@@ -7,10 +7,11 @@ spark = create_spark_session("filter")
 fire_df = read_dataset_into_df()
 
 
-few_fire_df = fire_df.select("IncidentNumber", "AvailableDtTm", "CallType").where(
-    col("CallType") != "Medical Incident"
-)
-few_fire_df.show(5, truncate=False)
+few_fire_df = fire_df.select("IncidentNumber", "AvailableDtTm", "CallType")\
+    .filter((col("CallType") != "Medical Incident") & col("AvailableDtTm").isNotNull())\
+    .sort(col("AvailableDtTm")).limit(50)
+
+few_fire_df.show(truncate=False)
 
 
 # Return number of distinct types of calls using countDistinct()
