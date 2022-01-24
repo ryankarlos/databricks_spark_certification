723fffced8b539bebe7b11174dfeaa67b7955101
Ryan Nazareth <ryankarlos@gmail.com>
2022-01-24 18:27:26 +0000

    add udf example

---
+++ spark_api/udf.py
@@ -0,0 +1,30 @@
+from spark_session import create_spark_session
+from pyspark.sql.functions import udf, pandas_udf, col
+from pyspark.sql.types import LongType
+import pandas as pd
+from timer import Timer
+from dataframe_api.io import read_dataset_into_df
+
+spark = create_spark_session("optimiser")
+
+
+@udf(LongType())
+def multiply(v: pd.Series, s: pd.Series) -> pd.Series:
+    return v * s
+
+
+@pandas_udf(LongType())
+def pandas_multiply(v: pd.Series, s: pd.Series) -> pd.Series:
+    return v * s
+
+if __name__ == "__main__":
+    file = "datasets/iot-devices/iot_devices.json"s
+    df = read_dataset_into_df(file, "json")
+
+    print("\n Using pandas udf:")
+    with Timer():
+        df.select("temp", "humidity", pandas_multiply(col("temp"), col("humidity")).alias("multiply")).show()
+
+    print("\n Using normal udf:")
+    with Timer():
+        df.select("temp","humidity", multiply(col("temp"), col("humidity")).alias("multiply")).show()
--- spark_session.py
+++ spark_session.py
@@ -7,5 +7,14 @@ from pyspark.sql import SparkSession
 
 # Create a SparkSession
 def create_spark_session(app_name):
-    spark = SparkSession.builder.appName(app_name).getOrCreate()
+    # increase spark driver memory (default 1gb) to avoid heap space warnings
+    spark = (
+        SparkSession.builder.appName(app_name)
+            .config("spark.sql.execution.arrow.pyspark.enabled", True)
+            .config("spark.driver.memory", "16G")
+            .config("spark.ui.showConsoleProgress", True)
+            .config("spark.sql.repl.eagerEval.enabled", True)
+            .config("spark.dynamicAllocation.enabled", True)
+            .getOrCreate()
+    )
     return spark
